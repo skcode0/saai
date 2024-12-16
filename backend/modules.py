143ef1888a71a19f0f6e90356cec634eb307d12f
skcode0 <51051626+skcode0@users.noreply.github.com>
2024-12-16 16:15:20 -0600

    updated model

--- backend/modules.py
+++ backend/modules.py
@@ -33,7 +33,7 @@ def analyze_sentiment(transcription):
     # sentiment_analyzer = pipeline("sentiment-analysis", model="joeddav/distilbert-base-uncased-go-emotions-student", return_all_scores=True)
 
     # my own fine-tuned model (from local files; can also be accessed publicly via Hugging Face)
-    sentiment_analyzer = pipeline("sentiment-analysis", model="../models/augmented_v2", return_all_scores=True)
+    sentiment_analyzer = pipeline("sentiment-analysis", model="sangkm/augmented-go-emotions-plus-other-datasets-fine-tuned-distilroberta-v2", return_all_scores=True)
 
     result = sentiment_analyzer(transcription)[0]
 
